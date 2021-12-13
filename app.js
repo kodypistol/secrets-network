0618644489fddfedfd3bea063cec290c1d2083a8
kodypistol <kodypistol@gmail.com>
2021-12-13 16:53:41 +0100

    Added submit post route. Fixing username bug with github and google connections

--- app.js
+++ app.js
@@ -38,7 +38,9 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  githubId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -65,7 +67,7 @@ passport.use(new GoogleStrategy({
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    User.findOrCreate({ googleId: profile.id, username: profile.displayName }, function (err, user) {
       return cb(err, user);
     });
   }
@@ -118,17 +120,29 @@ app.get("/register", function(req,res) {
 });
 
 app.get("/secrets", function(req,res) {
-  if (req.isAuthenticated()){
-    res.render("secrets");
-
-  } else {
-    res.redirect("/login");
-  }
+  User.find({"secret": {$ne: null}}, function (err, foundUsers){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
 });
 
 app.get("/logout", function(req,res){
   req.logout();
   res.redirect("/");
+});
+
+app.get("/submit", function(req,res){
+  if (req.isAuthenticated()){
+    res.render("submit");
+
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.post("/register", function(req,res){
@@ -141,6 +155,25 @@ app.post("/register", function(req,res){
       passport.authenticate("local")(req, res, function(){
         res.redirect("/secrets");
       });
+    }
+  });
+
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+  console.log(req.user.id);
+
+  User.findById(req.user.id, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
     }
   });
 
